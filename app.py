7671cfad0cfef8946a06046654be7a5472e692e3
nguyenducdo94 <nguyenducdo94@gmail.com>
2023-09-10 16:59:43 +0700

    add check os api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from selenium import webdriver
 import os
+import platform
+
 
 app = Flask(__name__)
 
@@ -30,3 +32,23 @@ def fetch_data():
         return jsonify({'title': title})
     except Exception as e:
         return jsonify({'error': str(e)})
+
+@app.route('/checkos')
+def check_os():
+    # Kiểm tra hệ điều hành
+    system_info = platform.system()
+    print(f'Hệ điều hành: {system_info}')
+
+    # Kiểm tra phiên bản hệ điều hành (chỉ áp dụng cho Linux)
+    if system_info == 'Linux':
+        try:
+            with open('/etc/os-release', 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if line.startswith('PRETTY_NAME='):
+                        _, os_name = line.split('=', 1)
+                        os_name = os_name.strip().strip('"')
+                        print(f'Phiên bản hệ điều hành: {os_name}')
+                        break
+        except FileNotFoundError:
+            pass
