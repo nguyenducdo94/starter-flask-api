5b0a635f5e865e107a6c122e4f0e9f734a4bf9e8
nguyenducdo94 <nguyenducdo94@gmail.com>
2023-09-10 16:38:21 +0700

    add new fetch data api

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from selenium import webdriver
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return 'Hello Aifone API'
+
+@app.route('/fetch_data', methods=['GET'])
+def fetch_data():
+    try:
+        url = request.args.get('url')  # Lấy URL từ tham số truy vấn
+        # Khởi tạo trình duyệt Selenium
+        driver = webdriver.Chrome()
+        driver.get(url)  # Mở URL trong trình duyệt
+
+        # Thực hiện các tác vụ Selenium ở đây
+        # Ví dụ: Lấy tiêu đề của trang
+        title = driver.title
+
+        driver.quit()
+
+        return jsonify({'title': title})
+    except Exception as e:
+        return jsonify({'error': str(e)})
