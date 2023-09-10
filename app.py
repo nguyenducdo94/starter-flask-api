3381a84e7d2ffa13cd5cdef28a24cde7efa01ccc
nguyenducdo94 <nguyenducdo94@gmail.com>
2023-09-10 16:53:35 +0700

    a

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ def hello_world():
 def fetch_data():
     try:
         url = request.args.get('url')  # Lấy URL từ tham số truy vấn
+
+        print(f'gotta url {url}')
         # Khởi tạo trình duyệt Selenium
         driver = webdriver.Chrome('./chromedriver')
+        print(f'created driver')
         driver.get(url)  # Mở URL trong trình duyệt
-
+        print(f'gotta to {url}')
         # Thực hiện các tác vụ Selenium ở đây
         # Ví dụ: Lấy tiêu đề của trang
         title = driver.title
