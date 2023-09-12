2fe4ce5890d65c267d37c7a14ef3702e8256724f
nguyenducdo94 <nguyenducdo94@gmail.com>
2023-09-12 22:10:22 +0700

    a

--- app.py
+++ app.py
@@ -36,12 +36,28 @@ def check_os():
 
 @app.route('/crawl')
 def scrape_and_render():
-    url = request.args.get("link")
+    # url = request.args.get("link")
 
-    content = requests.get(url)
+    # Create a string containing the cookie data
+    cookie_string = "wd=1920x931; datr=Y-D-ZIKxQXk1OXOM3LSKhaRq; sb=6bj_ZJ2mhN1QcYWnM8yoEae4; c_user=100002244977876; xs=27%3AY4h3YKNAhuDmlA%3A2%3A1694531343%3A-1%3A6381; fr=09HoYKJd0rVC7X3Vv.AWUxJv5WuccIKmyC3ScY9E956tQ.Bk_7jp.Fs.AAA.0.0.BlAH8S.AWVebDTlM8Y; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1694531352842%2C%22v%22%3A1%7D; useragent=TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzE"
+    # You can add more cookies as needed
+
+    # Split the cookie string into individual cookies
+    cookie_list = cookie_string.split('; ')
+
+    # Create a dictionary to store the cookies
+    cookies = {}
+
+    # Convert the list of cookies into a dictionary
+    for cookie in cookie_list:
+        key, value = cookie.split('=', 1)  # Split at the first '=' character
+        cookies[key] = value
+
+    # Make the request with the cookies
+    response = requests.get("https://facebook.com", cookies=cookies)
 
     # Gửi yêu cầu đến trang web
-    html_content = content.text
+    html_content = response.text
     
     # Return the HTML content
     return html_content
